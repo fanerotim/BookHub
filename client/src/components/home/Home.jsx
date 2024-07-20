73bfece366b414888f502f41809b9a86a704b7a8
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-20 19:35:40 +0100

    iteration of home page styles

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -4,11 +4,56 @@ const Home = () => {
     return (
         <main className='home__container'>
             <section className='home__text'>
-                <h1 className='home__text__main-heading'>Welcome to the BookHub!</h1>
-                <h2 className='home__text__subheading'>Our mission is to help readers find books that they would love</h2>
+                    <h1 className='home__text__main-heading'>Welcome to the BookHub!</h1>
+                    <h2 className='home__text__subheading'>Our mission is to provide information about books that have the power to change lives. You can scroll through a huge library of books and also have the option to join us</h2>
             </section>
 
             <section className='home__background__img'>
+            </section>
+
+            <section className='home__app__info'>
+
+                <div>
+                    <h1 className='home__app__info__heading'>Registered users can...</h1>
+                    <ul className='home__app__info__icons__list'>
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon">thumb_up</span>
+                            <li className='home__app__info__icons__list__item'>Like a book</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon">thumb_down</span>
+                            <li className='home__app__info__icons__list__item'>Dislike a book</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon">comment</span>
+                            <li className='home__app__info__icons__list__item'>Comment/Review a book</li>
+                        </div>
+                    </ul>
+
+                </div>
+
+                <div>
+                    <h1 className='home__app__info__heading'>Also...</h1>
+                    <ul className='home__app__info__icons__list'>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon">add</span>
+                            <li className='home__app__info__icons__list__item'>Add a book</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon">edit</span>
+                            <li className='home__app__info__icons__list__item'>Edit their own books</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon">delete</span>
+                            <li className='home__app__info__icons__list__item'>Delete any of their books with one click</li>
+                        </div>
+                    </ul>
+                </div>
             </section>
         </main>
     )
