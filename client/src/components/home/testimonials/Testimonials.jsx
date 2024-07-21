25d0690322d98077460c9e8d82c206016f72420e
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-21 18:07:43 +0100

    set up initial iteration of home page slider

--- client/src/components/home/testimonials/Testimonials.jsx
+++ client/src/components/home/testimonials/Testimonials.jsx
@@ -1,13 +1,49 @@
+import { useEffect, useState } from 'react';
 import './Testimonials.scss'
 import testimonialImg from './testimonial1.jpg';
 
+const testimonials = [
+    {text: 'I wanted to say that I am very happy to have found this website'},
+    {text: 'Yes, this seems to be working fine!'}
+]
+
 const Testimonials = () => {
+
+    const [index, setIndex] = useState(0);
+
+    function slideRight() {
+        setIndex(index => {
+            if (index + 1 === testimonials.length) {
+                return index = 0;
+            } else {
+                return index + 1;
+            }
+        });
+    }
+
+    function slideLeft() {
+        setIndex(index => {
+            if (index <= 0) {
+                
+                return index = testimonials.length - 1;
+            } else {
+                console.log('more than 0')
+                return index - 1;
+            }
+        })
+    }
+
+    useEffect(() => {
+        console.log(`re-render, ${index}`)
+    }, [index])
+
+
     return (
         <section className="testimonials">
             <h1 className='testimonials__heading'>What our users are Saying</h1>
 
-            <section className="testimonials__card">
-                <p className="testimonials__card__text">Since I found BookHub I have not been looking around for any other place for book suggestions. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, veritatis!</p>
+            <section className="testimonials__card" {...index % 2 === 0 ? {style:{backgroundColor: 'azure'}} : ''}>
+                <p className="testimonials__card__text">{testimonials[index].text}</p>
                 
                 <div className='testimonials__card__user__container'>
                     <img className='testimonials__card__user__container__img' src={testimonialImg} alt="image" />
@@ -15,6 +51,11 @@ const Testimonials = () => {
                     <span className='testimonials__card__user__container__quote__icon'></span>
                 </div>
             </section>
+
+            
+            <button onClick={slideLeft} className='testimonials__buttons testimonials__buttons__left'><span className="material-symbols-outlined">arrow_back</span></button>
+            <button onClick={slideRight} className='testimonials__buttons testimonials__buttons__right'><span className="material-symbols-outlined">arrow_forward</span></button>
+            
         </section>
     ) 
 }
