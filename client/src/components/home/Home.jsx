07f80c353136b162c2155d24b2c0899d5571f9fa
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-21 15:06:59 +0100

    create new component Features and remove some of the code on home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,60 +1,20 @@
 import './Home.scss';
+import Testimonials from './testimonials/Testimonials';
+import Features from './features/Features';
 
 const Home = () => {
     return (
         <main className='home__container'>
             <section className='home__text'>
-                    <h1 className='home__text__main-heading'>Welcome to BookHub!</h1>
-                    <h2 className='home__text__subheading'>Our mission is to provide information about books that have the power to change lives. You can scroll through a huge library of books and also have the option to join us</h2>
+                <h1 className='home__text__main-heading'>Welcome to BookHub!</h1>
+                <h2 className='home__text__subheading'>Our mission is to provide information about books that have the power to change lives. You can scroll through a huge library of books and also have the option to join us</h2>
             </section>
 
             <section className='home__background__img'>
             </section>
 
-            <section className='home__app__info'>
-
-                <div className='home__app__info__wrapper__one'>
-                    <h1 className='home__app__info__heading'>Registered users can...</h1>
-                    <ul className='home__app__info__icons__list'>
-                        <div className='home__app__info__item__wrapper'>
-                            <span className="material-symbols-outlined home__app__info__icons__list__icon">thumb_up</span>
-                            <li className='home__app__info__icons__list__item'>Like a book</li>
-                        </div>
-
-                        <div className='home__app__info__item__wrapper'>
-                            <span className="material-symbols-outlined home__app__info__icons__list__icon">thumb_down</span>
-                            <li className='home__app__info__icons__list__item'>Dislike a book</li>
-                        </div>
-
-                        <div className='home__app__info__item__wrapper'>
-                            <span className="material-symbols-outlined home__app__info__icons__list__icon">comment</span>
-                            <li className='home__app__info__icons__list__item'>Comment/Review a book</li>
-                        </div>
-                    </ul>
-
-                </div>
-
-                <div className='home__app__info__wrapper__two'>
-                    <h1 className='home__app__info__heading'>Also...</h1>
-                    <ul className='home__app__info__icons__list'>
-
-                        <div className='home__app__info__item__wrapper'>
-                            <span className="material-symbols-outlined home__app__info__icons__list__icon">add</span>
-                            <li className='home__app__info__icons__list__item'>Add a book</li>
-                        </div>
-
-                        <div className='home__app__info__item__wrapper'>
-                            <span className="material-symbols-outlined home__app__info__icons__list__icon">edit</span>
-                            <li className='home__app__info__icons__list__item'>Edit their own books</li>
-                        </div>
-
-                        <div className='home__app__info__item__wrapper'>
-                            <span className="material-symbols-outlined home__app__info__icons__list__icon">delete</span>
-                            <li className='home__app__info__icons__list__item'>Delete any of their books with one click</li>
-                        </div>
-                    </ul>
-                </div>
-            </section>
+            <Features/>
+            <Testimonials/>
         </main>
     )
 }
---
+++ client/src/components/home/features/Features.jsx
@@ -0,0 +1,52 @@
+import './Features.scss'
+
+const Features = () => {
+    return (
+        <section className='home__app__info'>
+
+                <h1 className='home__app__info__heading'>Some of our Features</h1>
+                <p className='home__app__info__subheading'>Registered users have access to the below functionalities. Don't worry though, you can browse through our library as a guest too</p>
+
+                <div className='home__app__info__features__wrapper'>
+                    <ul className='home__app__info__icons__list'>
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon home__app__info__icons__list__icon__like">thumb_up</span>
+                            <li className='home__app__info__icons__list__item'>Like a book</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon home__app__info__icons__list__icon__dislike">thumb_down</span>
+                            <li className='home__app__info__icons__list__item'>Dislike a book</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon home__app__info__icons__list__icon__comment">comment</span>
+                            <li className='home__app__info__icons__list__item'>Comment/Review a book</li>
+                        </div>
+                    </ul>
+
+                    <ul className='home__app__info__icons__list'>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon home__app__info__icons__list__icon__add">add</span>
+                            <li className='home__app__info__icons__list__item'>Add a book</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon home__app__info__icons__list__icon__edit">edit</span>
+                            <li className='home__app__info__icons__list__item'>Edit a book</li>
+                        </div>
+
+                        <div className='home__app__info__item__wrapper'>
+                            <span className="material-symbols-outlined home__app__info__icons__list__icon home__app__info__icons__list__icon__delete">delete</span>
+                            <li className='home__app__info__icons__list__item'>Delete a book with one click</li>
+                        </div>
+                    </ul>
+
+                </div>
+
+            </section>
+    )
+}
+
+export default Features;
