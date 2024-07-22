dcb1b6d6d31db5b76e321fca748acd1891b717f5
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-22 17:43:07 +0100

    set up initial style of login page

--- client/src/components/home/testimonials/Testimonials.jsx
+++ client/src/components/home/testimonials/Testimonials.jsx
@@ -1,17 +1,26 @@
 import { useEffect, useState } from 'react';
 import './Testimonials.scss'
 import testimonialImg from './testimonial1.jpg';
+import testimonialImg1 from './testimonial2.jpg'
 
 const testimonials = [
-    {text: 'I wanted to say that I am very happy to have found this website'},
-    {text: 'Yes, this seems to be working fine!'}
+    {
+        text: 'I wanted to say that I am very happy to have found this website',
+        name: 'P. Cholakov',
+        image: testimonialImg,
+    },
+    {
+        text: 'Yes, this seems to be working fine!',
+        name: 'M. Cholakova',
+        image: testimonialImg1
+    }
 ]
 
 const Testimonials = () => {
 
     const [index, setIndex] = useState(0);
 
-    function slideRight() {
+    function slideRight(e) {
         setIndex(index => {
             if (index + 1 === testimonials.length) {
                 return index = 0;
@@ -31,29 +40,37 @@ const Testimonials = () => {
         })
     }
 
-    useEffect(() => {}, [index])
+    useEffect(() => { }, [index])
 
 
     return (
         <section className="testimonials">
+            <p className='testimonials__heading__small'>Testimonials</p>
             <h1 className='testimonials__heading'>What our users are Saying</h1>
 
-            <section className="testimonials__card" {...index % 2 === 0 ? {style:{backgroundColor: 'azure'}} : ''}>
-                <p className="testimonials__card__text">{testimonials[index].text}</p>
-                
+            <section className="testimonials__card" >
+                {/* <p className="testimonials__card__text">{testimonials[index].text}</p> */}
+
+
                 <div className='testimonials__card__user__container'>
-                    <img className='testimonials__card__user__container__img' src={testimonialImg} alt="image" />
-                    <p className='testimonials__card__user__container__name'>P. Cholakov</p>
+                    <div className='testimonials__card__user__container__img__container'>
+                        <img className='testimonials__card__user__container__img' src={testimonials[index].image} alt="image" />
+                        <p className='testimonials__card__user__container__name'>{testimonials[index].name}</p>
+                    </div>
+
+                    <div className='testimonials__card__user__container__text__container'>
+                        <p className="testimonials__card__text">{testimonials[index].text}</p>
+                    </div>
                     <span className='testimonials__card__user__container__quote__icon'></span>
                 </div>
             </section>
 
-            
+
             <button onClick={slideLeft} className='testimonials__buttons testimonials__buttons__left'><span className="material-symbols-outlined">arrow_back</span></button>
             <button onClick={slideRight} className='testimonials__buttons testimonials__buttons__right'><span className="material-symbols-outlined">arrow_forward</span></button>
-            
+
         </section>
-    ) 
+    )
 }
 
 export default Testimonials;
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -2,7 +2,32 @@ import './Login.scss'
 
 const Login = () => {
     return (
-        <h1>Please Log in!</h1>
+
+        <div className='login__container'>
+
+            <section className='login__wrapper'>
+
+                <div className='login__wrapper__info__container'>
+                    <h1 className='login__wrapper__info__container__main__text'>Get full access to all functionalities on our website</h1>
+                </div>
+
+                <section className='login__form__container'>
+                    <h1 className='login__form__heading'>Log in</h1>
+
+                    <form className='login__form'>
+                        <label className='login__form__label' htmlFor="">Email</label>
+                        <input className='login__form__input' type="text" />
+
+                        <label className='login__form__label' htmlFor="">Password</label>
+                        <input className='login__form__input' type="password" />
+                        
+                        <button className='login__form__button'>Log in</button>
+                    </form>
+                </section>
+                
+            </section>
+
+        </div>
     )
 }
 
