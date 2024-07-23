5bee7c9e94272108aed6419d63cb42b59166efe4
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-23 20:19:49 +0100

    set up register page styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,21 @@ import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Footer from './components/footer/Footer'
 import Login from './components/login/Login'
-import { Routes, Route } from 'react-router-dom'
 import Register from './components/register/Register'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Header />
+
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/register' element={<Register/>}></Route>
       </Routes>
+      
       <Footer />
     </>
   )
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -2,8 +2,32 @@ import './Register.scss'
 
 const Register = () => {
     return (
-        <section>
-            <h1>Register</h1>
+        <section className='register__container'>
+            <div className='register__content__wrapper'>
+
+                <div className='register__content__wrapper__text'>
+                    <h1 className='register__content__wrapper__text__main'>Join the <span className='register__content__wrapper__text__main__logo'>BookHub</span> Community</h1>
+                    <h2 className='register__content__wrapper__text__description'>Be part of a community that celebrates stories and knowledge. Sign up now to start exploring a vast collection of books.</h2>
+                </div>
+
+                <div className='register__form__wrapper'>
+                    <h1 className='register__form__heading'>Sign up</h1>
+                    <form className='register__form' action="">
+                        <label className='register__form__label' >Name</label>
+                        <input className='register__form__input' type="text" />
+
+                        <label className='register__form__label' >Email</label>
+                        <input className='register__form__input' type="text" />
+
+                        <label className='register__form__label' >Password</label>
+                        <input className='register__form__input' type="text" />
+
+                        <label className='register__form__label'>Confirm Password</label>
+                        <input className='register__form__input' type="text" />
+                        <button className='register__form__button'>Submit</button>
+                    </form>
+                </div>
+            </div>
         </section>
     )
 }
