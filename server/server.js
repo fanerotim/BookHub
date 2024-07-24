1d1c4d184058b1df80e905ba64340a0f41654475
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-24 15:19:31 +0100

    set up server folder, install express

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+    console.log('this route was reached')
+    res.send('We start building our server today, but it does not hotreload')
+})
