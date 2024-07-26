38ec756550434f94a9ab5f91288b868ae066cff5
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-26 20:59:50 +0100

    leave TODO comments

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -4,6 +4,9 @@ export const apiService = async (method, path, data) => {
 
     const options = {};
 
+    // TODO: get access token and attach it to the req
+
+    //TODO: test if delete requests will cause any issues by having headers and body defined
     if (method !== 'GET') {
         options.method = method
         options.headers = {
@@ -12,15 +15,14 @@ export const apiService = async (method, path, data) => {
         options.body = JSON.stringify(data);
     }
 
-    
-        const response = await fetch(`${BASE_URL}${path}`, options);
-        const result = await response.json();
+    const response = await fetch(`${BASE_URL}${path}`, options);
+    const result = await response.json();
 
-        if (!response.ok) {
-            throw result;
-        }
+    if (!response.ok) {
+        throw result;
+    }
 
-        return result;
+    return result;
 }
 
 export const get = apiService.bind('', 'GET');
