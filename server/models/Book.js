1c55efb5324f024d0bd6bad3ded496a8825aef73
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-28 19:29:19 +0100

    set up book model

---
+++ server/models/Book.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imgUrl: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Book', bookSchema);
