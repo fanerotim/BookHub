b8b34e89624382a272d178cd67ae2192b75cbb5c
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-28 19:29:26 +0100

    set up book controller

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const bookService = require('../services/bookService');
 
 router.get('/', (req, res) => {
     return res.status(200).json({message: 'Successfully accessed the CATALOG route!'})
 })
 
-router.post('/add-new', (req, res) => {
-    return res.status(200).json({message: 'Successfully accessed the ADD-NEW route!'})
+router.post('/add-book', async (req, res) => {
+    const bookDetails = req.body;
+
+    // TODO: create middleware that checks if the user is authenticated
+
+    try {
+        const newBook = await bookService.create(bookDetails);
+        return res.status(200).json(newBook)
+    } catch(err) {
+        return res.status(409).json({message: err.message})
+    }
+    
 })
 
 module.exports = router;
