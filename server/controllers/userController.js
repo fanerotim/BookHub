05289bb6a1101b49049d8f4cd587ffe1e9c6e7bd
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-28 20:28:26 +0100

    protect routes with route guards

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,10 @@
+const isGuest = require('../middlewares/isGuest');
+const isUser = require('../middlewares/isUser');
 const userService = require('../services/userService');
 
 const router = require('express').Router();
 
-router.post('/register', async (req, res) => {
+router.post('/register', isUser, async (req, res) => {
     const userDetails = req.body;
 
     try {
@@ -16,7 +18,7 @@ router.post('/register', async (req, res) => {
     } 
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', isUser, async (req, res) => {
     const loginDetails = req.body;
 
     try {
@@ -28,7 +30,7 @@ router.post('/login', async (req, res) => {
     }  
 })
 
-router.post('/logout', (req, res) => {
+router.post('/logout', isGuest, (req, res) => {
     req.user = null;
 
     return res.status(200).json({message: 'User has successfully logged out'})
