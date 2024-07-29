09826d255c5ed13b49cabcbb46cdbab55edfb2f7
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-29 21:21:38 +0100

    set up library component

--- client/src/components/library/Library.jsx
+++ client/src/components/library/Library.jsx
@@ -1,17 +1,41 @@
 import './Library.scss';
 import LibraryCard from './library-card/LibraryCard';
+import { NavLink } from 'react-router-dom';
+import useLibrary from '../../hooks/useLibrary';
+import { useEffect, useState } from 'react';
 
 const Library = () => {
+
+    const [books, setBooks] = useState([])
+    
+    const {getAll} = useLibrary();
+
+    useEffect(() => {
+        (async() => {
+            const allBooks = await getAll();
+            setBooks((oldBooks) => allBooks);
+            
+        })()
+    }, [])
+
     return (
         <section className='library__container'>
             <div className='library__text__container'>
-                <h1 className='library__text__container__heading'>Welcome to <span className='library__text__container__logo'>BookHub's</span> Catalog</h1>
-                <p className='library__text__container__subheading__main'>Dive into the vast universe of books with BookHub!</p>
+                <h1 className='library__text__container__heading'><span className='library__text__container__logo'>Our </span>Library</h1>
+                <p className='library__text__container__subheading__main'>Dive into the vast universe of books</p>
                 <p className='library__text__container__subheading'>Our catalog is designed to be your ultimate destination for discovering, sharing, and discussing books of all kinds.</p>
             </div>
 
+            <ul className='library__subNavigation'>
+               <NavLink className='library__subNavigation__item'>All</NavLink>
+               <NavLink className='library__subNavigation__item'>Fiction</NavLink>
+               <NavLink className='library__subNavigation__item'>Crime</NavLink> 
+            </ul>
+
             <div className='library__card__container'>
-                <LibraryCard/>
+                {books.map((book) => (
+                    <LibraryCard book={book}/>
+                ))}
             </div>
         </section>
 
