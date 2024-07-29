d1e39b94814038a0f7cd807b8ddef2be904dca9c
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-29 21:21:56 +0100

    set up useLibrary hook

---
+++ client/src/hooks/useLibrary.js
@@ -0,0 +1,20 @@
+import apiService from "../services/apiService";
+
+const useLibrary = () => {
+
+    const getAll = async () => {
+        
+        try {
+            const allBooks = await apiService.get('/books/library');
+            return allBooks;
+        } catch(err) {
+            return err.message
+        }   
+    }
+
+    return {
+        getAll
+    }
+}
+
+export default useLibrary;
