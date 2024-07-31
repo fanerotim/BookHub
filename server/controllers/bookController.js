bae2e3e906d63139519ae10867506f0938e46413
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-31 16:07:15 +0100

    removed unnecessary comment

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -3,19 +3,18 @@ const isGuest = require('../middlewares/isGuest');
 const bookService = require('../services/bookService');
 
 router.get('/library', async (req, res) => {
-    
+
     try {
         const books = await bookService.getAll();
         return res.status(200).json(books);
     } catch(err) {
         return res.status(400).json({message: err.message})
     }
+
 })
 
 router.post('/add-book', isGuest, async (req, res) => {
     const bookDetails = req.body;
-
-    // TODO: create middleware that checks if the user is authenticated
 
     try {
         const newBook = await bookService.create(bookDetails);
@@ -23,6 +22,7 @@ router.post('/add-book', isGuest, async (req, res) => {
     } catch(err) {
         return res.status(409).json({message: err.message})
     }  
+
 })
 
 module.exports = router;
