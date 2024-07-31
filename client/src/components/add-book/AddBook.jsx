b07e17793bf267d15c5f90036cd77fae8ac129ee
Petar Cholakov <petar.cholakov30@gmail.com>
2024-07-31 20:02:29 +0100

    add default empty option for select

--- client/src/components/add-book/AddBook.jsx
+++ client/src/components/add-book/AddBook.jsx
@@ -65,6 +65,7 @@ const AddBook = () => {
                         name="genre"
                         id='genre'
                         onChange={handleChange}>
+                            <option selected></option>
                             <option value='Fiction'>Fiction</option>
                             <option value='Biography'>Biography</option>
                             <option value='Science-Fiction'>Science-Fiction</option>
