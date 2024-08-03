02fbc49bc23f75f03132fee01451b3cbff6e769c
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-03 20:51:31 +0100

    set up delete method in book service at backend

--- server/services/bookService.js
+++ server/services/bookService.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Book = require('../models/Book');
 
 exports.create = async (bookDetails) => {
-    
+
     const title = bookDetails.title;
 
-    const isAdded = await Book.findOne({title});
-    
+    const isAdded = await Book.findOne({ title });
+
     if (isAdded) {
         throw Error('This book is already added to our database. Please think of a new one.')
     }
@@ -21,11 +21,16 @@ exports.getAll = async () => {
 }
 
 exports.getOne = async (bookId) => {
-    const book = await Book.findOne({_id: bookId});
+    const book = await Book.findOne({ _id: bookId });
     return book;
 }
 
-exports.update = async(bookDetails) => {
+exports.update = async (bookDetails) => {
     const updatedBook = await Book.findByIdAndUpdate(bookDetails._id, bookDetails)
     return updatedBook;
+}
+
+exports.delete = async (bookId) => {
+    const deletedBook = await Book.findByIdAndDelete(bookId);
+    return deletedBook;
 }
