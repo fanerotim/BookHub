af75113aef7286c7c542e71eab411904c0a9e90a
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-03 20:52:05 +0100

    set up useDeleteBook hook

---
+++ client/src/hooks/useDeleteBook.js
@@ -0,0 +1,22 @@
+import { useParams } from "react-router-dom";
+import apiService from "../services/apiService";
+import { useNavigate } from "react-router-dom";
+
+const useDeleteBook = () => {
+    const {bookId} = useParams();
+    const navigate = useNavigate();
+
+    const deleteBook = async () => {
+
+        try {
+            const deletedBook = await apiService.deleteOne(`/books/${bookId}/delete`)
+            navigate('/library')
+        } catch(err) {
+            console.log({message: err.message})
+        }
+    }
+
+    return {deleteBook};
+}
+
+export default useDeleteBook;
