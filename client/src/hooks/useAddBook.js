c1bcca11a14ed375740365d8789046ac0c60ca4f
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-05 16:21:07 +0100

    update useAddBook hook

--- client/src/hooks/useAddBook.js
+++ client/src/hooks/useAddBook.js
@@ -1,23 +1,14 @@
-import { useState } from "react";
 import apiService from "../services/apiService";
-import { useNavigate } from 'react-router-dom';
+
 
 const useAddBook = () => {
 
-    const [error, setError] = useState(null)
-    const navigate = useNavigate();
-
     const add = async (values) => {
-        try {
-            const newBook = await apiService.post('/books/add-book', values);
-            navigate('/library')
-        } catch (err) {
-            setError((oldError) => err.message)
-            // console.log(err.message);
-        }
+
+        const newBook = await apiService.post('/books/add-book', values);
     }
 
-    return { add, error };
+    return { add };
 }
 
 export default useAddBook;
