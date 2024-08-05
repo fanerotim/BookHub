6b79d0276c7ec33e10d81fcb4bb849dc8f785aca
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-05 16:21:18 +0100

    update useEditBook hook

--- client/src/hooks/useEditBook.js
+++ client/src/hooks/useEditBook.js
@@ -1,23 +1,12 @@
 import apiService from "../services/apiService";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 
 const useEditBook = () => {
-    const navigate = useNavigate();
-    const [error, setError] = useState(null);
-
+    
     const update = async (bookDetails) => {
-        
-        try {
-            const updatedBook = await apiService.put(`/books/${bookDetails._id}/edit`, bookDetails)
-            navigate(`/library/${updatedBook._id}`)
-        } catch (err) {
-            setError((oldError) => err.message)
-            // console.log(err.message)
-        }
+        const updatedBook = await apiService.put(`/books/${bookDetails._id}/edit`, bookDetails) 
     }
 
-    return { update, error }
+    return { update }
 }
 
 export default useEditBook;
