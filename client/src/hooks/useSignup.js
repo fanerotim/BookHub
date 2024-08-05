4be68a24fd6ce11320055286fd036af2f3b1c358
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-05 16:21:35 +0100

    update useSignup hook

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -1,30 +1,20 @@
-import { useState } from 'react';
 import apiService from '../services/apiService'
 import { useAuthContext } from './useAuthContext';
-import { useNavigate } from 'react-router-dom';
 
 export const useSignup = () => {
 
-    const [error, setError] = useState('');
-    const {dispatch} = useAuthContext();
-    const navigate = useNavigate();
+    const { dispatch } = useAuthContext();
 
     const signup = async (values) => {
 
-        try {
-            const user = await apiService.post('/user/register', values);
+        const user = await apiService.post('/user/register', values);
 
-            // update local storage with new user details
-            localStorage.setItem('auth', JSON.stringify(user))
-            // update state with new user details
-            dispatch({type: 'LOGIN', payload: user})
+        // update local storage with new user details
+        localStorage.setItem('auth', JSON.stringify(user))
 
-            // upon success redirect to home page
-            navigate('/')
-        } catch (err) {
-            setError(err.message)
-        }
+        // update state with new user details
+        dispatch({ type: 'LOGIN', payload: user })
     }
 
-    return {signup, error}
+    return { signup }
 }
