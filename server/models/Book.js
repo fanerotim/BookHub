76d60b6a192c5a15e8ba8dde84f5b6f24f091920
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-05 14:56:29 +0100

    adjust Book and User models

--- server/models/Book.js
+++ server/models/Book.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
-        require: true
+        required: true
     },
     author: {
         type: String,
