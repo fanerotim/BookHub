164007b7110183a31ffa115f42bf6d700680afc5
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-06 13:12:05 +0100

    initial styles of profile component

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 
 const Profile = () => {
 
-    const {getUser} = useProfile();
+    const { getUser } = useProfile();
     const [user, setUser] = useState(null)
 
     useEffect(() => {
@@ -15,9 +15,38 @@ const Profile = () => {
     }, [])
 
     return (
-        <>
-            <h1>Welcome to Profile page</h1>
-        </>
+        <section className='profile__container'>
+            
+            <div className='profile__information'>
+            <h1>Account details</h1>
+                <div>
+
+                </div>
+
+                <div className='profile__information__username__container'>
+                    <span className='profile__information__username__tag'>Username</span>
+                    <h1 className='profile__information__username'>{user?.username}</h1>
+                    
+                </div>
+
+                <div className='profile__information__email__container'>
+                    <span className='profile__information__email__tag'>Email</span>
+                    <h1 className='profile__information__email'>{user?.email}</h1>
+                </div>
+
+                <p>Liked books: {user && user.likedBooks.length}</p>
+                <p>Added books: {user && user.addedBooks.length}</p>
+            </div>
+
+            <div className='profile__likedAthletes__container'>
+
+            </div>
+
+            <div className='profile__addedAthletes__container'>
+
+            </div>
+
+        </section>
     )
 }
 
