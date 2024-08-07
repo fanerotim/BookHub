1f2cf7b7ba5e1ccf620df71373841ab5ab558fe6
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-07 19:06:10 +0100

    adjust styling of delete book modal

--- client/src/components/library/delete-modal/DeleteBookModal.jsx
+++ client/src/components/library/delete-modal/DeleteBookModal.jsx
@@ -2,16 +2,16 @@ import { useEffect } from 'react';
 import './DeleteBookModal.scss'
 import useDeleteBook from '../../../hooks/useDeleteBook';
 
-const DeleteBookModal = ({props}) => {
+const DeleteBookModal = ({ props, deleteModalItemDefinition }) => {
 
-    const {deleteBook} = useDeleteBook();
+    const { deleteBook } = useDeleteBook();
 
     useEffect(() => {
         // disable scroll when modal shows (mount)
         document.body.style.overflowY = 'hidden';
- 
+
         // enable scroll when modal is closed (unmount)
-        return () => {  
+        return () => {
             document.body.style.overflowY = 'scroll'
         }
     }, [])
@@ -22,7 +22,6 @@ const DeleteBookModal = ({props}) => {
     }
 
     function handleApprove() {
-        console.log('handle approve')
         deleteBook();
     }
 
@@ -31,7 +30,7 @@ const DeleteBookModal = ({props}) => {
             <div className='delete__modal'>
                 <h1 className='delete__modal__text'>Are you sure you want to delete this book entry from <span className='delete__modal__text__logo'>BookHub</span>? This action cannot be undone.</h1>
                 <p className='delete__modal__text__notice'>By clicking <span className='delete__modal__text__notice__delete'>Delete</span>, the book entry will be permanently removed from our library.</p>
-                
+
                 <div className='delete__modal__button__container'>
                     <button onClick={handleApprove} className='delete__modal__button__container__approve delete__modal__button__container__button'>Delete</button>
                     <button onClick={handleReject} className='delete__modal__button__container__reject delete__modal__button__container__button'>Cancel</button>
