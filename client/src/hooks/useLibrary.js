0ae920610c7843421d7d2edc0d5315e958974b61
Petar Cholakov <petar.cholakov30@gmail.com>
2024-08-07 19:38:07 +0100

    fix issue with useLibrary page

--- client/src/hooks/useLibrary.js
+++ client/src/hooks/useLibrary.js
@@ -1,6 +1,11 @@
 import apiService from "../services/apiService";
+import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 const useLibrary = () => {
+
+    const {dispatch} = useAuthContext();
+    const navigate = useNavigate();
 
     const getAll = async () => {   
 
@@ -8,6 +13,13 @@ const useLibrary = () => {
             const allBooks = await apiService.get('/books/library');
             return allBooks;
         } catch(err) {
+            // if token is changed manually e.g. token is invalid then:
+            //1. clear localStorage
+            localStorage.removeItem('auth');
+
+            //2. logout user
+            dispatch({type: 'LOGOUT', payload: null})
+            navigate('/login')
             return err.message
         }   
     }
