2bab645fff4f2dffe7431cad6b889f5aa840db2e
Petar Cholakov <petar.cholakov30@gmail.com>
2024-10-19 21:25:11 +0100

    update app url in expressConfig

--- server/config/expressConfig.js
+++ server/config/expressConfig.js
@@ -8,7 +8,7 @@ const expressConfig = (app) => {
     app.use(express.json())
 
     app.use(cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://book-hub.fanerotim.com',
         credentials: true
     }))
 
