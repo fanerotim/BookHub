3cb4248e5e8267e32aee1ca4a25056e411ed3830
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-07 20:58:35 +0000

    set loader to register page

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -4,6 +4,7 @@ import { useSignup } from '../../hooks/useSignup';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import Loader from '../loader/Loader'
 
 const initialValues = {
     username: '',
@@ -17,6 +18,7 @@ const Register = () => {
     const { signup } = useSignup();
     const [error, setError] = useState(null)
     const navigate = useNavigate();
+    const [registering, setRegistering] = useState(false)
 
     const { values, handleChange } = useForm(initialValues)
 
@@ -35,10 +37,11 @@ const Register = () => {
         }
 
         try {
+            setRegistering(() => true)
             const signUpReq = await signup(values);
             // redirect on success
             navigate('/')
-        } catch(err) {
+        } catch (err) {
             setError((oldError) => err.message)
         }
     }
@@ -52,45 +55,52 @@ const Register = () => {
                     <h2 className='register__content__wrapper__text__description'>Be part of a community that celebrates stories and knowledge. Sign up now to start exploring a vast collection of books.</h2>
                 </div>
 
+
+
                 <div className='register__form__wrapper'>
                     <h1 className='register__form__heading'>Sign up</h1>
-                    <form className='register__form' onSubmit={submitHandler}>
-                        <label className='register__form__label'>Username</label>
-                        <input
-                            className='register__form__input'
-                            name='username'
-                            value={values.username}
-                            onChange={handleChange}
-                            type="text" />
 
-                        <label className='register__form__label'>Email</label>
-                        <input
-                            className='register__form__input'
-                            name='email'
-                            value={values.email}
-                            onChange={handleChange}
-                            type="email" />
+                    {registering
+                        ? <div className='loader'>
+                        </div>
+                        : <form className='register__form' onSubmit={submitHandler}>
+                            <label className='register__form__label'>Username</label>
+                            <input
+                                className='register__form__input'
+                                name='username'
+                                value={values.username}
+                                onChange={handleChange}
+                                type="text" />
 
-                        <label className='register__form__label' >Password</label>
-                        <input
-                            className='register__form__input'
-                            name='password'
-                            value={values.password}
-                            onChange={handleChange}
-                            type="password" />
+                            <label className='register__form__label'>Email</label>
+                            <input
+                                className='register__form__input'
+                                name='email'
+                                value={values.email}
+                                onChange={handleChange}
+                                type="email" />
 
-                        <label className='register__form__label'>Confirm Password</label>
-                        <input
-                            className='register__form__input'
-                            name='rePassword'
-                            value={values.rePassword}
-                            onChange={handleChange}
-                            type="password" />
+                            <label className='register__form__label' >Password</label>
+                            <input
+                                className='register__form__input'
+                                name='password'
+                                value={values.password}
+                                onChange={handleChange}
+                                type="password" />
+
+                            <label className='register__form__label'>Confirm Password</label>
+                            <input
+                                className='register__form__input'
+                                name='rePassword'
+                                value={values.rePassword}
+                                onChange={handleChange}
+                                type="password" />
                             <p className='register__form__login__info'>Already registered?</p>
                             <Link className='register__form__login__link' to='/login'>Please click here to login</Link>
-                        <button className='register__form__button'>Submit</button>
-                        {error && (<p className='register__form__error__message'>{error}</p>)}
-                    </form>
+                            <button className='register__form__button'>Submit</button>
+                            {error && (<p className='register__form__error__message'>{error}</p>)}
+                        </form>}
+
                 </div>
             </div>
         </section>
