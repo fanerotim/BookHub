036a8e5972600b25ca7e3c24319a685b8af59a62
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-12 20:50:59 +0000

    add heading to quotes page

--- client/src/components/quotes/Quotes.jsx
+++ client/src/components/quotes/Quotes.jsx
@@ -2,7 +2,16 @@ import './Quotes.scss'
 
 const Quotes = () => {
     return (
-        <h1>Quotes page</h1>
+        
+        <div className='quotes__container'>
+            <h1 className='quotes__container__heading'><span className='quotes__container__heading__effect'>Our</span> Quotes</h1>
+
+
+            <section>
+                
+            </section>
+
+        </div>
     )
 }
 
