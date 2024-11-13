291264fc418458762b8e1eca3fa222230cd4e945
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-13 20:35:09 +0000

    add subheading on quotes page

--- client/src/components/quotes/Quotes.jsx
+++ client/src/components/quotes/Quotes.jsx
@@ -6,9 +6,10 @@ const Quotes = () => {
         <div className='quotes__container'>
             <h1 className='quotes__container__heading'><span className='quotes__container__heading__effect'>Our</span> Quotes</h1>
 
+            <h2 className='quotes__container__subheading'>BookHub's collection of words worth remembering. Add your favorite quotes and let readers discover new gems.</h2>
 
             <section>
-                
+
             </section>
 
         </div>
