5125293a7252d3fb04034113b153b96f97f7f55f
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-17 09:36:14 +0000

    set up grid layout for quote cards

--- client/src/components/quotes/Quotes.jsx
+++ client/src/components/quotes/Quotes.jsx
@@ -4,17 +4,19 @@ import db from './db';
 
 const Quotes = () => {
     return (
-        
+
         <div className='quotes__container'>
             <h1 className='quotes__container__heading'><span className='quotes__container__heading__effect'>Our</span> Quotes</h1>
 
             <h2 className='quotes__container__subheading'>BookHub's collection of words worth remembering. Add your favorite quotes and let readers discover new gems.</h2>
 
-            <section className='quotes__container__quote__card'>
-                {db.map((q, index) => (
-                    <QuotesCard q={q} index={index} key={index}/>
-                ))}
-            </section>
+            <div className='quotes__container__card__container'>
+                <section className='quotes__container__quote__card'>
+                    {db.map((q, index) => (
+                        <QuotesCard q={q} index={index} key={index} />
+                    ))}
+                </section>
+            </div>
 
         </div>
     )
