8c7c2a3554dadc74c6b7aa07dfa6e16335b86c98
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-22 17:32:36 +0000

    pass prev pathname and q data to quote details page

--- client/src/components/quotes/quotes-card/QuotesCard.jsx
+++ client/src/components/quotes/quotes-card/QuotesCard.jsx
@@ -1,16 +1,19 @@
 import styles from './QuotesCard.module.scss';
 import { FacebookShareButton, FacebookIcon } from 'react-share';
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
+
 const QuotesCard = ({ q, index }) => {
 
     const shareUrl = 'https://book-hub.fanerotim.com/library';
+    const location = useLocation();
 
     return (
         <section className={
             `${styles.quotes__container} 
             ${index % 2 === 0 ? styles.cardA : styles.cardB} 
             ${index === 0 ? `${styles.first__card__animation}` : ''}`}>
-            <Link className={styles.linkStyleReset} to={`/quotes/${q._id}`}>
+            <Link state={{q, location}} className={styles.linkStyleReset} to={`/quotes/${q._id}`}>
                 <h1 className={styles.quotes__container__quote}>
                     {q.quote}
                 </h1>
