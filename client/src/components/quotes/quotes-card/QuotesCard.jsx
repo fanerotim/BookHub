a2b60eb41e21aeda527f56f3ec8ca911703d1721
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-23 09:59:02 +0000

    add more btn to quotes-card

--- client/src/components/quotes/quotes-card/QuotesCard.jsx
+++ client/src/components/quotes/quotes-card/QuotesCard.jsx
@@ -6,22 +6,26 @@ const QuotesCard = ({ q, index }) => {
 
     const location = useLocation();
 
-    return (
-        <section className={
-            `${styles.quotes__container} 
-            ${index % 2 === 0 ? styles.cardA : styles.cardB} 
-            ${index === 0 ? `${styles.first__card__animation}` : ''}`}>
-            <Link 
-            state={{q, location}} 
-            className={styles.linkStyleReset} 
-            to={`/quotes/${q._id}`}>
-                <h1 className={styles.quotes__container__quote}>
-                    {q.quote}
-                </h1>
 
-                <h2 className={styles.quotes__container__author}>
-                    {q.author}
-                </h2>
+    return (
+        <section
+            className={
+            `${styles.quotes__container} 
+            ${index % 2 === 0 ? styles.cardA : styles.cardB}`}>
+                
+            <h1 className={styles.quotes__container__quote}>
+                {q.quote}
+            </h1>
+
+            <h2 className={styles.quotes__container__author}>
+                {q.author}
+            </h2>
+
+            <Link 
+            className={`${styles.linkStyleReset}`}
+            state={{ q, location }}
+            to={`/quotes/${q._id}`}>
+                More
             </Link>
         </section>
     )
