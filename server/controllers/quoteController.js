d8a8eb38cf623c26d3dafa9b797806e8b736e75b
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-24 21:06:43 +0000

    set up quoteController

---
+++ server/controllers/quoteController.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+router.post('/add', async (req, res) => {
+
+    const newQUote = req.body;
+    
+    try {
+
+    } catch(err) {
+        return err;
+    }
+})
+
+module.exports = router;
