05fe2550bd748fe030631bdc377f57c2c7331ab8
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-29 15:20:28 +0000

    remove unnecessary file

--- client/src/components/quotes/Quotes.jsx
+++ client/src/components/quotes/Quotes.jsx
@@ -1,9 +1,21 @@
 import './Quotes.scss'
 import QuotesCard from './quotes-card/QuotesCard';
-import db from './db';
 import Search from '../search/Search';
+import { useEffect, useState } from 'react';
+import useQuotes from '../../hooks/useQuotes';
+import Loader from '../loader/Loader';
 
 const Quotes = () => {
+
+    const [quotes, setQuotes] = useState([]);
+    const { getAllQuotes } = useQuotes();
+
+    useEffect(() => {
+        (async () => {
+            const allQuotes = await getAllQuotes()
+            setQuotes(allQuotes);
+        })()
+    }, [])
 
     return (
 
@@ -11,18 +23,24 @@ const Quotes = () => {
             <h1 className='quotes__container__heading'><span className='quotes__container__heading__effect'>Our</span> Quotes</h1>
 
             <h2 className='quotes__container__subheading'>BookHub's collection of words worth remembering. Add your favorite quotes and let readers discover new gems.</h2>
-            
-            <Search/>
+
+            <Search />
 
             <div className='quotes__container__card__container'>
-                <section className='quotes__container__quote__card'>
-                    {db.map((q, index) => (
-                        <QuotesCard 
-                        q={q} 
-                        index={index} 
-                        key={index} />
-                    ))}
-                </section>
+                {quotes.length > 0
+                    ?
+                    <section className='quotes__container__quote__card'>
+                        {quotes.map((q, index) => (
+                            <QuotesCard
+                                q={q}
+                                index={index}
+                                key={index} />
+                        ))}
+                    </section>
+                    :
+                    <Loader />
+                }
+
             </div>
 
         </div>
