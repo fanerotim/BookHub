a658e0060c26e06051753c3776c82949b9e6c6e2
Petar Cholakov <petar.cholakov30@gmail.com>
2024-11-29 15:20:38 +0000

    create useQuotes hook

---
+++ client/src/hooks/useQuotes.js
@@ -0,0 +1,21 @@
+import apiService from '../services/apiService';
+
+const useQuotes = () => {
+
+    const getAllQuotes = async (url) => {
+        
+        try {
+            const quotes = await apiService.get('/quotes');
+            console.log(quotes)
+            return quotes;
+        } catch (err) {
+            return err;
+        }
+    }
+
+    return {
+        getAllQuotes
+    }
+}
+
+export default useQuotes;
