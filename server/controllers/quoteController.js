6029ff93074e2942384f8ff9378395ee767ccec3
Petar Cholakov <petar.cholakov30@gmail.com>
2024-12-06 21:57:52 +0000

    create quoteController

--- server/controllers/quoteController.js
+++ server/controllers/quoteController.js
@@ -12,6 +12,15 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/search/:searchQuery', async (req, res) => {
+    const searchQuery = req.params.searchQuery;
+
+    console.log('searchQuery is', searchQuery);
+
+    
+    return res.status(200).json({success: 'request successfully completed'})
+})
+
 router.post('/add', async (req, res) => {
 
     const quoteData = req.body;
