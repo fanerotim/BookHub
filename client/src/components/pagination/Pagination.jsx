35d7029133622472f740d430b9c492d924d73038
Petar Cholakov <petar.cholakov30@gmail.com>
2024-12-09 20:52:52 +0000

    not fully working, but an idea on how to make page numbers change dynamically on click

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './Pagination.module.scss'
 
 const Pagination = ({
@@ -5,16 +6,30 @@ const Pagination = ({
     handlePageNumberClick,
     currentPage }) => {
 
+    const [pageNumbers, setPageNumbers] = useState(pagesCount); 
+
     const nextPageHandler = () => {
         if (currentPage + 1 <= pagesCount.length) {
             handlePageNumberClick(currentPage + 1);
         }
+
+     
+        const newPageNumbers = pageNumbers;
+        const firstPageNumber = newPageNumbers.shift();
+        newPageNumbers.push(firstPageNumber);
+        
+        setPageNumbers((prevPageNums) => newPageNumbers);
     }
 
     const prevPageHandler = () => {
         if (currentPage - 1 > 0) {
             handlePageNumberClick(currentPage - 1);
         }
+
+        const newPageNumbers = pageNumbers;
+        const lastPageNumber = newPageNumbers.pop();
+        newPageNumbers.push(lastPageNumber);
+        setPageNumbers(newPageNumbers);
     }
 
     console.log(pagesCount)
@@ -24,22 +39,25 @@ const Pagination = ({
             <ul
                 className={styles.paginationList}>
                 {
-                    pagesCount.map(i =>
-                         i < 3 ? 
-                        <li
-                            className={`${styles.paginationList__item} ${currentPage == i ? `${styles.paginationList__currentPage}` : ''}`}
-                            onClick={() => handlePageNumberClick(i)}
-                            key={i}>
-                            {i}
+                    pagesCount.map((i, index) =>
+                        index < 2 ?
+                            <li
+                                className={`${styles.paginationList__item} ${currentPage == i ? `${styles.paginationList__currentPage}` : ''}`}
+                                onClick={() => handlePageNumberClick(i)}
+                                key={i}>
+                                {pageNumbers[index]}
 
-                        </li>
-                        : i === 4 
-                        ? <li>...</li> 
-                        : i === 5 
-                        ? <li
-                            className={`${styles.paginationList__item} ${currentPage == i ? `${styles.paginationList__currentPage}` : ''}`}
-                        >5
-                        </li> : '')
+                            </li>
+                            : i === 4
+                                ? <li>...</li>
+                                : i === 5
+                                    ? <li
+                                        className={`${styles.paginationList__item} ${currentPage == i ? `${styles.paginationList__currentPage}` : ''}`}
+                                        onClick={() => handlePageNumberClick(i)}
+                                        key={i}
+                                    >{pageNumbers[index]}
+                                    </li> 
+                                    : '')
                 }
             </ul>
 
